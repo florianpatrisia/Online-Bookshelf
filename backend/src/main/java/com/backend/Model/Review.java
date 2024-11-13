68066c2572cc524f3677883fd35f319be91e1432
Decea Cezara <deceacezara2003@gmail.com>
2024-11-13 16:04:23 +0200

    feat(backend): delete comments

--- backend/src/main/java/com/backend/Model/Review.java
+++ backend/src/main/java/com/backend/Model/Review.java
@@ -14,35 +14,31 @@ import java.util.Date;
 @Data
 public class Review {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "review_id")
-	private Long reviewId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long reviewId;
 
-	@ManyToOne
-	@JoinColumn(name = "book_id", nullable = false)
-	private Book book;
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
 
-	@ManyToOne
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
-	@Temporal(TemporalType.DATE)
-	private Date date;
+    @Temporal(TemporalType.DATE)
+    private Date date;
 
-	private Integer rating;
+    private Integer rating;
 
-	private String description;
+    private String description;
 
-	@Override
-	public String toString() {
-		return "Review{" + "id=" + reviewId + ", book=" + book.getTitle() + // afișăm
-																			// titlul
-																			// cărții
-																			// pentru
-																			// simplitate
-				", user=" + user.getUsername() + // afișăm username-ul utilizatorului
-				", date=" + date + ", rating=" + rating + ", description='" + description + '\'' + '}';
-	}
+    @Override
+    public String toString() {
+        return "Review{" + "id=" + reviewId + ", book=" + book.getTitle() +
+                ", user=" + user.getUsername() +
+                ", date=" + date + ", rating=" + rating + ", description='" + description + '\'' + '}';
+    }
 
 }
