f593e3d7cc524ce4eb66182af7e3d01a28339f23
Decea Cezara <deceacezara2003@gmail.com>
2024-11-25 00:38:19 +0200

    feat(backend): resolve conflicts

--- backend/src/main/java/com/backend/Controller/BookController.java
+++ backend/src/main/java/com/backend/Controller/BookController.java
@@ -1,5 +1,6 @@
 package com.backend.Controller;
 
+import com.backend.Coverter.BookConverter;
 import com.backend.Model.Book;
 import com.backend.DTO.BookDTO;
 import com.backend.Service.BookService;
@@ -14,10 +15,8 @@ import java.util.Optional;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/books")
 public class BookController {
-
-	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
 
 	private final BookService bookService;
 
@@ -25,20 +24,27 @@ public class BookController {
 		this.bookService = bookService;
 	}
 
-	@GetMapping("/user/books")
-	public ResponseEntity<List<Book>> getAllBooks() {
-		logger.info("Getting all books");
+	@GetMapping
+	public ResponseEntity<List<BookDTO>> getAllBooks() {
 		List<Book> books = bookService.getAllBooks();
-		return ResponseEntity.ok(books);
+		List<BookDTO> bookDTOs = books.stream().map(BookConverter::to).toList();
+		return ResponseEntity.ok(bookDTOs);
 	}
 
-	@GetMapping("/user/{id}")
-	public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+	@GetMapping("/{id}")
+	public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
 		Optional<Book> book = bookService.getBookById(id);
-		return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+		if (book.isPresent()) {
+			BookDTO bookDTO = BookConverter.to(book.get());
+			return ResponseEntity.ok(bookDTO);
+		}
+		else {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
 	}
 
-	@PostMapping("/admin/books")
+	@PostMapping
 	public ResponseEntity<String> createBook(@ModelAttribute BookDTO bookDTO) {
 		if (bookDTO.getTitle().isEmpty() || bookDTO.getAuthor().isEmpty() || bookDTO.getDescription().isEmpty()
 				|| bookDTO.getPrice() == null || bookDTO.getImage().isEmpty() || bookDTO.getRating() == null
@@ -46,42 +52,33 @@ public class BookController {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Some fields are empty!");
 		}
 
-		Book savedBook = new Book();
-		savedBook.setTitle(bookDTO.getTitle());
-		savedBook.setAuthor(bookDTO.getAuthor());
-		savedBook.setDescription(bookDTO.getDescription());
-		savedBook.setPrice(bookDTO.getPrice());
-		savedBook.setImage(bookService.saveFileToAWSS3Bucket(bookDTO.getImage()));
-		savedBook.setRating(bookDTO.getRating());
-		savedBook.setAvailableCount(bookDTO.getAvailableCount());
-		savedBook.setCategory(bookDTO.getCategory());
+		String imageUrl = bookService.saveFileToAWSS3Bucket(bookDTO.getImage());
 
-		bookService.saveBook(savedBook);
+		Book book = BookConverter.from(bookDTO, imageUrl);
+		bookService.saveBook(book);
 		return ResponseEntity.ok("Book saved successfully");
 	}
 
-	@PutMapping("/admin/books/{id}")
+	@PutMapping("/{id}")
 	public ResponseEntity<String> updateBook(@PathVariable Long id, @ModelAttribute BookDTO bookDTO) {
 		Optional<Book> existingBook = bookService.getBookById(id);
 
 		if (existingBook.isPresent()) {
 			if (bookDTO.getTitle().isEmpty() || bookDTO.getAuthor().isEmpty() || bookDTO.getDescription().isEmpty()
-					|| bookDTO.getPrice() == null || bookDTO.getImage().isEmpty() || bookDTO.getRating() == null
-					|| bookDTO.getAvailableCount() == null || bookDTO.getCategory().isEmpty()) {
+					|| bookDTO.getPrice() == null || bookDTO.getRating() == null || bookDTO.getAvailableCount() == null
+					|| bookDTO.getCategory().isEmpty()) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Some fields are empty!");
 			}
 
-			Book updatedBook = existingBook.get();
-			updatedBook.setTitle(bookDTO.getTitle());
-			updatedBook.setAuthor(bookDTO.getAuthor());
-			updatedBook.setDescription(bookDTO.getDescription());
-			updatedBook.setPrice(bookDTO.getPrice());
-			updatedBook.setImage(bookService.saveFileToAWSS3Bucket(bookDTO.getImage()));
-			updatedBook.setRating(bookDTO.getRating());
-			updatedBook.setAvailableCount(bookDTO.getAvailableCount());
-			updatedBook.setCategory(bookDTO.getCategory());
+			String imageUrl = existingBook.get().getImage(); // Default to existing URL
+			if (bookDTO.getImage() != null && !bookDTO.getImage().isEmpty()) {
+				imageUrl = bookService.saveFileToAWSS3Bucket(bookDTO.getImage());
+			}
 
+			Book updatedBook = BookConverter.from(bookDTO, imageUrl);
+			updatedBook.setBookId(id);
 			bookService.saveBook(updatedBook);
+
 			return ResponseEntity.ok("Book updated successfully");
 		}
 		else {
@@ -89,47 +86,12 @@ public class BookController {
 		}
 	}
 
-	@DeleteMapping("/admin/books/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<String> deleteBook(@PathVariable Long id) {
 		Optional<Book> book = bookService.getBookById(id);
 		if (book.isPresent()) {
 			bookService.deleteBook(id);
 			return ResponseEntity.ok("Book deleted successfully");
-		}
-		else {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book not found");
-		}
-	}
-
-	@PatchMapping("/admin/books/{id}")
-	public ResponseEntity<String> partiallyUpdateBook(@PathVariable Long id, @ModelAttribute BookDTO bookDTO) {
-		Optional<Book> existingBook = bookService.getBookById(id);
-
-		if (existingBook.isPresent()) {
-			Book bookToUpdate = existingBook.get();
-
-			if (bookDTO.getTitle() != null)
-				bookToUpdate.setTitle(bookDTO.getTitle());
-			if (bookDTO.getAuthor() != null)
-				bookToUpdate.setAuthor(bookDTO.getAuthor());
-			if (bookDTO.getDescription() != null)
-				bookToUpdate.setDescription(bookDTO.getDescription());
-			if (bookDTO.getPrice() != null)
-				bookToUpdate.setPrice(bookDTO.getPrice());
-			if (bookDTO.getRating() != null)
-				bookToUpdate.setRating(bookDTO.getRating());
-			if (bookDTO.getAvailableCount() != null)
-				bookToUpdate.setAvailableCount(bookDTO.getAvailableCount());
-			if (bookDTO.getCategory() != null)
-				bookToUpdate.setCategory(bookDTO.getCategory());
-
-			if (bookDTO.getImage() != null && !bookDTO.getImage().isEmpty()) {
-				String imageUrl = bookService.saveFileToAWSS3Bucket(bookDTO.getImage());
-				bookToUpdate.setImage(imageUrl);
-			}
-
-			bookService.saveBook(bookToUpdate);
-			return ResponseEntity.ok("Book partially updated successfully");
 		}
 		else {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Book not found");
