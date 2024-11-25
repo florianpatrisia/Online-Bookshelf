45f65eda0bc82b92e8bcddf80626a3502be4ed60
Decea Cezara <deceacezara2003@gmail.com>
2024-11-25 17:22:09 +0200

    feat(backend): remove unused files

--- backend/src/main/java/com/backend/Service/UserService.java
+++ backend/src/main/java/com/backend/Service/UserService.java
@@ -2,42 +2,32 @@ package com.backend.Service;
 
 import com.backend.Model.User;
 import com.backend.Repository.UserRepository;
-import com.backend.Utils.AESCipher;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.IvParameterSpec;
 
 @Service
 public class UserService {
 
-	private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-	private final PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-	private final SecretKey secretKey;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) throws Exception {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-	private final IvParameterSpec iv;
+    public User saveUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            throw new IllegalArgumentException("Username already exists");
+        }
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Email already exists");
+        }
 
-	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) throws Exception {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-		this.secretKey = AESCipher.generateSecretKey();
-		this.iv = AESCipher.generateIv();
-	}
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
 
-	public User saveUser(User user) {
-		if (userRepository.findByUsername(user.getUsername()).isPresent()) {
-			throw new IllegalArgumentException("Username already exists");
-		}
-		if (userRepository.findByEmail(user.getEmail()).isPresent()) {
-			throw new IllegalArgumentException("Email already exists");
-		}
-
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
-
-		return userRepository.save(user);
-	}
+        return userRepository.save(user);
+    }
 
 }
